a4cb510bdb513860120dd18dfca17da9d9ec0d3e
toby676 <tjcmui@gmail.com>
2016-06-08 09:29:16 +0100

    changed names

--- spec/features/forms_with_name_spec.rb
+++ spec/features/forms_with_name_spec.rb
@@ -3,9 +3,9 @@ feature 'fill in forms with names' do
 	scenario 'enter names and they should be displayed below' do
 		visit('/')
 		fill_in :name1, with: "Toby"
-		fill_in :name2, with: "Apoorva"
+		fill_in :name2, with: "Hassan"
 		click_button 'Submit'
-		expect(page).to have_content "Toby v Apoorva"
+		expect(page).to have_content "Toby v Hassan"
 	end
 
 end
