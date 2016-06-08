877f296e65e764bd895ad36c857d417fb04a6058
Hassan Radheyyan <hassanradheyyan@Hassans-MacBook-Pro.local>
2016-06-08 15:42:26 +0100

    added more functions to player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,19 @@
 class Player
 
-  attr_reader :name
+  attr_reader :name, :hit_points
+  DEFAULT_HIT_POINTS=100
 
   def initialize(name)
     @name = name
+    @hit_points = DEFAULT_HIT_POINTS
+  end
+
+  def attack(player)
+    player.receive_hit
+  end
+
+  def receive_hit
+    @hit_points -= 10
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,6 +1,7 @@
 describe Player do
   
   subject(:player) { described_class.new(:name) }
+  let(:player2) { described_class.new(:name2) }
 
   describe '#name' do
 
@@ -9,5 +10,18 @@ describe Player do
     end
 
   end
- 
+
+  describe '#hit_points' do
+    it 'returns the default hit points' do
+      expect(player.hit_points).to eq Player::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'reduces the other player\'s hit points' do
+      player.attack(player2)
+      expect(player2.hit_points).to eq 90
+    end
+  end
+
 end
